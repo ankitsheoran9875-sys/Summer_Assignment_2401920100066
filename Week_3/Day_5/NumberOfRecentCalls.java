67d983bad23e55c5d7691b2d32429b2c96f578d0
ankitsheoran9875-sys <ankitsheoran9875@gmail.com>
2026-07-01 20:13:24 +0530

    Update NumberOfRecentCalls.java

--- Week_3/Day_5/NumberOfRecentCalls.java
+++ Week_3/Day_5/NumberOfRecentCalls.java
@@ -1,1 +1,48 @@
+class RecentCounter {
+      class Node{
+       int val; 
+       Node next;
+       public Node(){ }
+       public Node(int val){
+           this.val = val;
+       }
+   }
+   Node head;
+   Node tail;
+   int headVal, tailVal;
+    
+    public RecentCounter() {
+        
+    }
+    
+    public int ping(int t) {
+      Node add = new Node(t);
+      if(tail == null){
+          head = tail = add;
+          tailVal++;
+          return 1;
+      }
+      tail.next = add;
+      tailVal++;
+      tail = add;
 
+    if(tail.val - head.val > 3000){
+      while(tail.val - head.val>3000){
+          head = head.next;
+          headVal++;
+      }
+    }
+    if(head == tail){
+          headVal = 0;
+          tailVal = 1;
+          return 1;
+    }
+     return tailVal-headVal;
+    }
+}
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter obj = new RecentCounter();
+ * int param_1 = obj.ping(t);
+ */
